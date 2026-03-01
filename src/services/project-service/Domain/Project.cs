1ddbcdd3809864943cdb2530f7e6e0744d65820c
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-01 16:11:39 +0300

    test(domain): add project creation test and minimal code skeleton (Red case)

---
+++ src/services/project-service/Domain/Project.cs
@@ -0,0 +1,10 @@
+namespace project_service.Domain;
+
+public class Project
+{
+    public readonly string Name;
+
+    public Project(string name)
+    {
+    }
+}
--- tests/services/project-service.Tests/Domain/ProjectTests.cs
+++ tests/services/project-service.Tests/Domain/ProjectTests.cs
@@ -1,5 +1,20 @@
-namespace project_services.Tests.Domain;
+using FluentAssertions;
+using project_service.Domain;
+
+namespace project_service.Tests.Domain;
 public class ProjectTests
 {
-    
+   [Fact]
+   public void CreateProject_WithValidName_ShouldCreateProject()
+    {
+        //Arrange
+        var name = "Yaz lab Project 1";
+
+        //Act
+        var project = new Project(name);
+
+        //Assert
+        project.Name.Should().Be(name);
+        
+    } 
 }
