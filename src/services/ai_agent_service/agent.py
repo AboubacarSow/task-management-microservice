1a3266484c6ae1b27a472a5145005451f213d9a8
Hasibullah Mohmand <155886773+hasibullahmohmand@users.noreply.github.com>
2026-03-02 13:25:28 +0300

    refactor: update generate_description implementation

--- src/services/ai_agent_service/agent.py
+++ src/services/ai_agent_service/agent.py
@@ -1,3 +1,26 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+
 class Agent:
+    def __init__(self):
+        self.llm = ChatOllama(model="llama3.2")
     def generate_description(self, project_name):
-        return "Test project description."
+        prompt = ChatPromptTemplate([
+            ("system",
+             """You are a professinal project description writer.
+             Your job is to write a one paragraph sutible, relevant and supported porject description based on the project name.
+             Don't output anything else just the generated paragraph.
+             """),
+            ("human",
+             """Project name: {project_name}
+             """)
+        ])
+        
+        generate_description_chain = prompt | self.llm
+        response = generate_description_chain.invoke(project_name)
+        return response.content
+    
+if __name__ == "__main__":
+    a = Agent()
+    response =a.generate_description("AI in healthcare")
+    print(response)
