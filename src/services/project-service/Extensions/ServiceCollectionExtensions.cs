13cacc8d02b4bfc3f9d1ab0434544e9e67a27227
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-17 21:08:37 +0300

    Add index on TaskCollection and register collections

---
+++ src/services/project-service/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using project_service.Data.Utilities;
+using project_service.Projects.Models;
+using Task = project_service.Tasks.Models.Task;
+namespace project_service.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCollections(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddSingleton<IMongoCollection<Project>>(scope =>
+        {
+            var settings = scope.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+
+            var client = new MongoClient(settings.ConnectionStrings);
+            var database = client.GetDatabase(settings.Database);
+            return database.GetCollection<Project>(settings.ProjectCollection);
+        });
+
+        services.AddSingleton<IMongoCollection<Task>>(scope =>
+        {
+            var settings = scope.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+
+            var client = new MongoClient(settings.ConnectionStrings);
+            var database = client.GetDatabase(settings.Database);
+            return database.GetCollection<Task>(settings.TaskCollection);
+        });
+        return services;
+    }
+}
