6078ff9f3a64eea32493bb2044dbaa8af15c1de4
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-18 06:06:01 +0300

    test(projectRepository):add test and minimal code implement for editProject method

--- src/services/project-service/Data/Repositories/IProjectRepository.cs
+++ src/services/project-service/Data/Repositories/IProjectRepository.cs
@@ -1,5 +1,3 @@
-using MongoDB.Driver;
-using MongoDB.Driver.Linq;
 using project_service.Projects.Models;
 
 namespace project_service.Data.Repositories;
@@ -7,27 +5,7 @@ namespace project_service.Data.Repositories;
 public interface IProjectRepository
 {
     Task AddAsync(Project project);
+    Task EditAsync(Project prt);
     Task<List<Project>> GetAllByUserId(Guid userId);
-    Task<Project> GetByIdAsync(Guid id);
-}
-
-public class ProjectRepository(IMongoCollection<Project> collection) : IProjectRepository
-{
-    private readonly IMongoCollection<Project> _collection = collection;
-
-    public async Task AddAsync(Project project)
-    {
-        await _collection.InsertOneAsync(project);
-    }
-
-    public async Task<List<Project>> GetAllByUserId(Guid userId)
-    {
-        return await _collection.Find(p=>p.CreatedByUser==userId)
-                            .ToListAsync();
-    }
-
-    public async Task<Project> GetByIdAsync(Guid id)
-    {
-        return await _collection.Find(p=>p.Id==id).FirstOrDefaultAsync();
-    }
+    Task<Project?> GetByIdAsync(Guid id);
 }
