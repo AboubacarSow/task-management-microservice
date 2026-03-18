385bdc2d156eff7f6b518de1de5c3c0c8e2d4bca
Hasibullah Mohmand <155886773+hasibullahmohmand@users.noreply.github.com>
2026-03-18 15:11:56 +0300

    Refactor mongodb connection

--- src/services/user_service/database/mongo.py
+++ src/services/user_service/database/mongo.py
@@ -2,14 +2,18 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv("../.env")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, "../.env")
+
+load_dotenv(ENV_PATH)
 
 MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DB_NAME")
 USER_COLLECTION = os.getenv("USER_COLLECTION")
 
+if not MONGO_URL or not DB_NAME or not USER_COLLECTION:
+    raise ValueError("Missing environment variables")
+
 client = MongoClient(MONGO_URL)
-
 db = client[DB_NAME]
-
 user_collection = db[USER_COLLECTION]
