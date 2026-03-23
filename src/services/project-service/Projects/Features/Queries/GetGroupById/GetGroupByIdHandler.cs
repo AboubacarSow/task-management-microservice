f0b3a1dc2addc6772fc5698bb97511264cbe293f
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-23 04:41:58 +0300

    test(Get-handler):add test and implement for getgroup and getpeople

---
+++ src/services/project-service/Projects/Features/Queries/GetGroupById/GetGroupByIdHandler.cs
@@ -0,0 +1,52 @@
+using project_service.Data.Repositories;
+using MediatR;
+using project_service.Commons.Exceptions;
+using project_service.Projects.Models;
+
+namespace project_service.Projects.Features.Queries.GetGroupById;
+
+
+public record GetGroupByIdQuery(Guid UserId,Guid ProjectId):IRequest<List<Guid>>;
+public class GetGroupByIdHandler(IProjectRepository repository, ILogger<GetGroupByIdHandler> logger)
+        : IRequestHandler<GetGroupByIdQuery, List<Guid>>
+{
+    private readonly IProjectRepository _repository = repository;
+    private readonly ILogger<GetGroupByIdHandler> _logger = logger;
+
+    public async Task<List<Guid>> Handle(GetGroupByIdQuery request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(
+            "User {UserId} is requesting Group for Project {ProjectId}",
+            request.UserId,
+            request.ProjectId);
+
+        var project = await _repository.GetByIdAsync(request.ProjectId);
+
+        if (project is null)
+        {
+            _logger.LogWarning(
+                "Project not found. ProjectId: {ProjectId}",
+                request.ProjectId);
+
+            throw new NotFoundException(nameof(Project), request.ProjectId.ToString());
+        }
+
+        if (request.UserId != project.OwnerId)
+        {
+            _logger.LogWarning(
+                "Unauthorized access to Group. User {UserId} is not owner of Project {ProjectId}",
+                request.UserId,
+                request.ProjectId);
+
+            throw new UnauthorizedAccessException();
+        }
+
+        _logger.LogInformation(
+            "Group retrieved successfully for Project {ProjectId} by Owner {UserId}",
+            request.ProjectId,
+            request.UserId);
+
+
+        return [.. project.Group];
+    }
+}
