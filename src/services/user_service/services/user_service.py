79b2ec3838beb748df1721e36b3cec1d8c611b87
Hasibullah Mohmand <155886773+hasibullahmohmand@users.noreply.github.com>
2026-03-24 20:37:44 +0300

    feat: implemented minimal authenticate_user endpoint to pass the test

--- src/services/user_service/services/user_service.py
+++ src/services/user_service/services/user_service.py
@@ -45,3 +45,14 @@ class UserService:
             raise ValueError("User with this id does not exist")
         
         return user
+    
+    async def authenticate_user(self, email: str, password: str):
+        user = await self.user_repository.get_user_by_email(email)
+
+        if not user:
+            return None
+
+        if not verify_password(password, user.password):
+            return None
+
+        return user
