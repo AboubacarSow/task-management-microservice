5b735f520cebd3e58318187cf4f2d89758582009
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-25 00:15:28 +0300

    CancelTaskHandler: Add test and minimal implementation for handler

---
+++ src/services/project-service/Tasks/Features/Commands/CancelTask/CancelTaskHandler.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace project_service.Tasks.Features.Commands.CancelTask;
+
+public record CancelTaskCommand(Guid CurrentUserId, Guid TaskId):IRequest<Unit>;
+
+public class CancelTaskHandler(ITaskRepository taskRepo,IProjectRepository projectRepo,
+ILogger<CancelTaskHandler> logger) : IRequestHandler<CancelTaskCommand,Unit>
+{
+   private readonly ITaskRepository _taskRepository = taskRepo;
+   private readonly IProjectRepository _projectRepository = projectRepo;
+   private readonly ILogger<CancelTaskHandler> _logger = logger;
+
+    public async Task Handle(CancelTaskCommand command, CancellationToken none)
+    {
+        var task = await _taskRepository.GetByIdAsync(command.TaskId);
+
+        if(task == null)
+        {
+            _logger.LogWarning(
+                "Task with Id:{TaskId} NOT_FOUND",
+                command.TaskId);
+            throw new NotFoundException(nameof(TaskItem),
+                command.TaskId.ToString());
+        }
+
+        var project = await _projectRepository.GetByIdAsync(task.ProjectId);
+        if(project == null)
+        {
+            _logger.LogWarning(
+                "Project with Id:{ProjectId} NOT_FOUND for Task with Id : {TaskId}",
+                task.ProjectId,task.Id);
+
+            throw new NotFoundException(nameof(Project),task.ProjectId.ToString());
+        }
+
+        if (task.CreatedByUser !=  command.CurrentUserId &&
+            !project.IsInGroup(command.CurrentUserId))
+        {
+            _logger.LogWarning(
+                "User {UserId} not authorized to perform this operation :{Operation}",
+                command.CurrentUserId, "EDIT_TASK");
+
+            throw new ForbiddenException(command.CurrentUserId.ToString(), "EDIT_TASK");
+        }
+
+        var owner = task.CreatedByUser;
+
+        task.Cancel();
+
+        await _taskRepository.EditAsync(task);
+
+        if (owner != command.CurrentUserId)
+            _logger.LogInformation("User in Group updated Task :{TaskId} successfully",
+                command.TaskId);
+                
+        return Unit.Value;
+    }
+}
