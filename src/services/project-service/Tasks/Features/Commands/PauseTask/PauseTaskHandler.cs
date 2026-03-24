a9f9f4c3a63c99af63ef1730379a2d1a26a0bfe8
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-25 01:17:17 +0300

    Add Handler test for PauseTask

---
+++ src/services/project-service/Tasks/Features/Commands/PauseTask/PauseTaskHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace project_service.Tasks.Features.Commands.PauseTask;
+
+
+public record PauseTaskCommand(Guid id, Guid userId, string V);
+public class PauseTaskHandler
+{
+    public PauseTaskHandler(ITaskRepository object1, IProjectRepository object2, ILogger<PauseTaskHandler> object3)
+    {
+    }
+
+    public async Task Handle(PauseTaskCommand command, CancellationToken none)
+    {
+        throw new NotImplementedException();
+    }
+}
