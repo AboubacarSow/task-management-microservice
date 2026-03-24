ab283e7bb48faa4ed78bc9b95aa48218b280cca8
ABOUBACAR SOW <175620651+AboubacarSow@users.noreply.github.com>
2026-03-24 13:29:46 +0300

    feat(UnAssignTaskHandler):implement handler to pass tests and refactor test code

--- src/services/project-service/Tasks/Features/Commands/UnAssignTask/UnAssignTaskHandler.cs
+++ src/services/project-service/Tasks/Features/Commands/UnAssignTask/UnAssignTaskHandler.cs
@@ -3,15 +3,31 @@
 namespace project_service.Tasks.Features.Commands.UnAssignTask;
 
 
-public class UnAssignTaskCommand(Guid TaskId,Guid CurrentUserId);
-public class UnAssignTaskHandler
+public record UnAssignTaskCommand(Guid TaskId,Guid CurrentUserId):IRequest<Unit>;
+public class UnAssignTaskHandler(ITaskRepository taskRepository,ILogger<UnAssignTaskHandler> logger)
+    : IRequestHandler<UnAssignTaskCommand, Unit>
 {
-    public UnAssignTaskHandler(ITaskRepository object1, IProjectRepository object2)
+    private readonly ITaskRepository _taskRepository = taskRepository;
+    private readonly ILogger<UnAssignTaskHandler> _logger = logger;
+    public async Task<Unit> Handle(UnAssignTaskCommand command, CancellationToken none)
     {
-    }
+        var task = await _taskRepository.GetByIdAsync(command.TaskId);
 
-    public async Task Handle(UnAssignTaskCommand command, CancellationToken none)
-    {
-        throw new NotImplementedException();
+        if(task == null)
+        {
+            _logger.LogWarning("Task with Id:{TaskId} not found",command.TaskId);
+            throw new NotFoundException(nameof(command), command.TaskId.ToString());
+        }
+        if(task.CreatedByUser != command.CurrentUserId)
+        {
+            _logger.LogWarning("User {UserId} not allowed to perform UNASSIGN_TASK",command.CurrentUserId);
+            throw new ForbiddenException(command.CurrentUserId.ToString(), "UNASSIGN_TASK");
+        }
+        var old_user = task.AssignedToUser;
+        task.UnAssign();
+        await _taskRepository.EditAsync(task);
+
+        _logger.LogInformation("Task unassigned from User :{UserId} successfully", old_user);
+        return Unit.Value;
     }
 }
--- tests/services/project-service.Tests/Tasks/Features/Commands/UnAssignTask/UnAssignTaskHandlerTests.cs
+++ tests/services/project-service.Tests/Tasks/Features/Commands/UnAssignTask/UnAssignTaskHandlerTests.cs
@@ -5,29 +5,26 @@ namespace project_service.Tests.Tasks.Features.Commands.UnAssignTask;
 public class UnassignTaskHandlerTests
 {
     private readonly Mock<ITaskRepository> _taskRepo = new();
-    private readonly Mock<IProjectRepository> _projectRepo = new();
+    private readonly Mock<ILogger<UnAssignTaskHandler>> _loggerMock = new();
     private readonly UnAssignTaskHandler _handler;
-
+    private readonly Guid ownerId = Guid.NewGuid();
+    private readonly Guid projectId = Guid.NewGuid();
     public UnassignTaskHandlerTests()
     {
-        _handler = new UnAssignTaskHandler(_taskRepo.Object, _projectRepo.Object);
+        _handler = new UnAssignTaskHandler(_taskRepo.Object,_loggerMock.Object);
     }
 
     [Fact]
     public async Task Should_Unassign_When_User_Is_AssignedUser()
     {
-        var userId = Guid.NewGuid();
-        var projectId = Guid.NewGuid();
-        var project = new Project("test", userId);
-        var task = new TaskItem("task", project.Id, userId);
+      
+        var task = new TaskItem("task", projectId, ownerId);
 
         var other_user= Guid.NewGuid();
         task.AssignTo(other_user);
 
         _taskRepo.Setup(x => x.GetByIdAsync(task.Id)).ReturnsAsync(task);
-        _projectRepo.Setup(x => x.GetByIdAsync(project.Id)).ReturnsAsync(project);
-
-        var command = new UnAssignTaskCommand(task.Id, userId);
+        var command = new UnAssignTaskCommand(task.Id,ownerId);
 
         await _handler.Handle(command, CancellationToken.None);
 
@@ -38,17 +35,12 @@ public class UnassignTaskHandlerTests
     [Fact]
     public async Task Should_Throw_When_User_Is_NotTaskOwner()
     {
-        var owner = Guid.NewGuid();
         var other = Guid.NewGuid();
 
-        var project = new Project("test", Guid.NewGuid());
-        project.AddUserToGroup(other);
-
-        var task = new TaskItem("task", project.Id, owner);
+        var task = new TaskItem("task", projectId, ownerId);
         task.AssignTo(other);
 
         _taskRepo.Setup(x => x.GetByIdAsync(task.Id)).ReturnsAsync(task);
-        _projectRepo.Setup(x => x.GetByIdAsync(project.Id)).ReturnsAsync(project);
 
         var command = new UnAssignTaskCommand(task.Id, Guid.NewGuid());
 
@@ -57,37 +49,16 @@ public class UnassignTaskHandlerTests
         await action.Should().ThrowAsync<ForbiddenException>();
     }
 
-    [Fact]
-    public async Task Should_Throw_When_Not_Authorized()
-    {
-        var owner = Guid.NewGuid();
-        var outsider = Guid.NewGuid();
-
-        var project = new Project("test", owner);
-        var task = new TaskItem("task", project.Id, owner);
-        task.AssignTo(owner);
-
-        _taskRepo.Setup(x => x.GetByIdAsync(task.Id)).ReturnsAsync(task);
-        _projectRepo.Setup(x => x.GetByIdAsync(project.Id)).ReturnsAsync(project);
-
-        var command = new UnAssignTaskCommand(task.Id, outsider);
-
-        var act = () => _handler.Handle(command, CancellationToken.None);
-
-        await act.Should().ThrowAsync<ForbiddenException>();
-    }
 
     [Fact]
     public async Task Should_Throw_When_Task_Not_Assigned()
     {
         var userId = Guid.NewGuid();
-        var project = new Project("test", userId);
-        //project.AddToGroup(userId);
+        var projectId = Guid.NewGuid();
 
-        var task = new TaskItem("task", project.Id, userId);
+        var task = new TaskItem("task", projectId, userId);
 
         _taskRepo.Setup(x => x.GetByIdAsync(task.Id)).ReturnsAsync(task);
-        _projectRepo.Setup(x => x.GetByIdAsync(project.Id)).ReturnsAsync(project);
 
         var command = new UnAssignTaskCommand(task.Id, userId);
 
