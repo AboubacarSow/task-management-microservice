9d36de9fd82ece0f44451496fcbd2be196199be3
Hasibullah Mohmand <155886773+hasibullahmohmand@users.noreply.github.com>
2026-03-25 21:05:15 +0300

    .env files added

--- src/services/ai_agent_service/agents/agent.py
+++ src/services/ai_agent_service/agents/agent.py
@@ -3,18 +3,27 @@ from langchain_core.prompts import ChatPromptTemplate
 from typing import List, Optional
 from pydantic import BaseModel, Field
 import logging
+from dotenv import load_dotenv
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_PATH = os.path.join(BASE_DIR, "../.env")
+
+load_dotenv(ENV_PATH)
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 
 logger = logging.getLogger(__name__)
 
 class Agent:
     def __init__(self):
-        self.llm = ChatOllama(model="llama3.2")
+        self.llm = ChatOllama(model=OLLAMA_MODEL, base_url=os.getenv("OLLAMA_BASE_URL"))
         logger.info("Agent initialized with model: llama3.2")
     def generate_description(self, project_name: str) -> str:
         logger.info(f"Generating description for project: {project_name}")
         prompt = ChatPromptTemplate.from_messages([
             ("system",
-             """You are a professinal project description writer.
+             """You are a professional project description writer.
              Your job is to write a one paragraph sutible, relevant and supported porject description based on the project name.
              Don't output anything else just the generated paragraph.
              """),
