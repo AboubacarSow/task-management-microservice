a7f1b86e81cc33c3b251e432114299e4f7e8d2c0
Hasibullah Mohmand <155886773+hasibullahmohmand@users.noreply.github.com>
2026-03-27 11:41:54 +0300

    refactor: claim fields updated

--- src/services/ai_agent_service/api/api.py
+++ src/services/ai_agent_service/api/api.py
@@ -22,33 +22,33 @@ class AgentApi:
     def register_routes(self):
         @self.app.post("/api/agent/generate_description")
         def generate_description(request: GenerateDescriptionInput, payload: dict = Depends(get_current_user)) -> GenerateDescriptionOutput:
-            logger.info(f"Generate description request received from user_id: {payload.get("id")}")
+            logger.info(f"Generate description request received from user_id: {payload.get("sub")}")
 
             response = self.agent.generate_description(request.project_name)
             
-            self.repo.log_response(LogResponse(user_id=payload["id"], task_type="Generate Description",
+            self.repo.log_response(LogResponse(user_id=payload["sub"], task_type="Generate Description",
                                                project_name=request.project_name, response=response))
             
             return GenerateDescriptionOutput(project_description=response)
 
         @self.app.post("/api/agent/suggest_tasks")
         def suggest_tasks(request: SuggestTasksInput, payload: dict = Depends(get_current_user)) -> SuggestTasksOutput:
-            logger.info(f"Suggest tasks request received from user_id: {payload.get("id")}")
+            logger.info(f"Suggest tasks request received from user_id: {payload.get("sub")}")
             
             response = self.agent.suggest_tasks(request.project_name,request.project_description)
             
-            self.repo.log_response(LogResponse(user_id=payload["id"], task_type="Suggest Tasks", project_name=request.project_name,
+            self.repo.log_response(LogResponse(user_id=payload["sub"], task_type="Suggest Tasks", project_name=request.project_name,
                                                project_description=request.project_description, response=response))
             
             return {"suggested_tasks": response}
         
         @self.app.post("/api/agent/refine_project_name")
         def refine_project_name(request: RefineProjectNameInput, payload: dict = Depends(get_current_user)) -> RefineProjectNameOutput:
-            logger.info(f"Refine project name request received from user_id: {payload.get("id")}")
+            logger.info(f"Refine project name request received from user_id: {payload.get("sub")}")
             
             response = self.agent.refine_project_name(request.project_name)
             
-            self.repo.log_response(LogResponse(user_id=payload["id"], task_type="Refine Project Name",
+            self.repo.log_response(LogResponse(user_id=payload["sub"], task_type="Refine Project Name",
                                                project_name=request.project_name, response=response))
             
             return RefineProjectNameOutput(refined_project_name=response)
