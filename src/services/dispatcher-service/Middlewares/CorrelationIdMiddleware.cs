5758312d1fb17dcdf7eb27fb535c8acf7c5287ba
hasibullahmohmand <hasibullah.mohmand14@gmail.com>
2026-03-28 23:08:14 +0300

    feat(dispatcher): implement CorrelationIdMiddleware

--- src/services/dispatcher-service/Middlewares/CorrelationIdMiddleware.cs
+++ src/services/dispatcher-service/Middlewares/CorrelationIdMiddleware.cs
@@ -2,14 +2,22 @@
 
 namespace dispatcher_service.Middlewares;
 
-public class CorrelationIdMiddleware
+public class CorrelationIdMiddleware(RequestDelegate next)
 {
-    public CorrelationIdMiddleware(RequestDelegate next)
-    {
-    }
+    private readonly RequestDelegate _next = next;
+
+    private readonly string CorrelationIdHeader = "X-Correlation-ID";
 
     public async Task InvokeAsync(HttpContext context)
     {
-        throw new NotImplementedException();
+        var correlationId = context.Request.Headers[CorrelationIdHeader].ToString();
+
+        if (string.IsNullOrEmpty(correlationId))
+            correlationId = Guid.NewGuid().ToString();
+
+        context.Request.Headers[CorrelationIdHeader]  = correlationId;
+        context.Response.Headers[CorrelationIdHeader] = correlationId;
+
+        await _next(context);
     }
 }
