f5ddb42736ea728ed0caf94278bb542c452b5651
Hasibullah Mohmand <155886773+hasibullahmohmand@users.noreply.github.com>
2026-04-04 12:32:15 +0300

    bug fixed

--- src/services/project-service/Program.cs
+++ src/services/project-service/Program.cs
@@ -87,7 +87,6 @@ builder.Services.AddGrpcClient<TaskInfo.TaskInfoClient>(o =>
     }).AddInterceptor<ServiceTokenInterceptor>();
 
 var app = builder.Build();
-app.UseMetrics();
 
 app.MapGrpcService<ProjectsGrpcService>();
 
